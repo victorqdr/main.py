9f58e1925bc0d19a6919ac98d6ed1b2c90890cdf
VictorDev <victorqdr918@gmail.com>
2024-04-22 22:03:11 -0300

    hasghdgd

---
+++ main.py
@@ -0,0 +1,11 @@
+from recurso.funcoes import limparTela,aguarde,calculaMedia
+
+limparTela()
+print(calculaMedia(8,6)) 
+print("primeira frase")
+aguarde
+limparTela()
+print("segunda frase")
+aguarde
+limparTela()
+print("terceira frase ")
---
+++ recurso/funcoes.py
@@ -0,0 +1,9 @@
+import os
+import time
+def limparTela():
+    os.system("cls")
+def aguarde(tempo):
+        time.sleep(tempo)
+def calculaMedia(nota1,nota2):
+      media =(nota1+nota2)/2 
+      return media  
